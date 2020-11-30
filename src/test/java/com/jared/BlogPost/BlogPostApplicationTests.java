96233d033f7a845e9e78f6309e543cab36bb6fe8
Jared Langson <langson.jared@gmail.com>
2020-11-29 23:23:07 -0500

    Added delete confirmation popup

---
+++ src/test/java/com/jared/BlogPost/BlogPostApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jared.BlogPost;
+
+import com.jared.blog.BlogProject1Application;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = BlogProject1Application.class)
+class BlogPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
